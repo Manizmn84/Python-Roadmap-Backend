569f4efa43f15c3ad4168c0513bac414bacbb43b
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-17 04:35:23 -0700

    feat: inital Library Entity

--- Course2/first_project/first_app/models.py
+++ Course2/first_project/first_app/models.py
@@ -9,3 +9,21 @@ class Person(models.Model) :
     age = models.IntegerField(default=18)
     country = models.CharField(max_length=10 , null=True)
     gender = models.CharField(choices=GENDER_CHOICE , default="m")
+
+class Author(models.Model) :
+    name = models.CharField(max_length=100)
+
+class Book(models.Model) :
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+class Profile(models.Model) :
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+
+class Library(models.Model) :
+    number = models.PositiveIntegerField()
+
+class Personal(models.Model) :
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library , on_delete=models.CASCADE)
