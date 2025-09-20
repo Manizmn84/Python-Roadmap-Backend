7c8ec661d40f0ea95bb2af84ca9b89f5b10d1eda
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-20 05:39:34 -0700

    feat:initial query for aggregate

--- Course2/first_project/first_app/queries.py
+++ Course2/first_project/first_app/queries.py
@@ -25,3 +25,23 @@ def all_mobiles_with_brand_of(brand_name) -> QuerySet:
 def all_available_mobiles_with_price_in_range(min_price, max_price) -> int:
     query = Mobile.objects.filter(price__lte = max_price , price__gte = min_price , is_available=True)
     return query.count() 
+
+from django.db.models import Sum, Min, Max, Avg
+def total_value_of_products():
+    query = Mobile.objects.aggregate(total_value= Sum('price'))
+    return query
+
+
+def total_value_of_products_with_brand_of(brand_name):
+    query = Mobile.objects.filter(brand__name= brand_name).aggregate(total_value= Sum('price'))
+    return query
+
+
+def most_expensive_cheapest_price_with_nationality_of(nationality):
+    query = Mobile.objects.filter(brand__nationality= nationality).aggregate(cheap= Min('price'), expensive= Max('price'))
+    return query
+
+
+def display_size_avg_in_available_mobiles_between_price(minimum, maximum):
+    query = Mobile.objects.filter(price__gte= minimum, price__lte= maximum, is_available= 1).aggregate(avg= Avg('display_size'))
+    return query
