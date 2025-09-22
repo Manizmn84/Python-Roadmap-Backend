94e8719899020c6a1dd0d22d41a2ba8120e76f68
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-22 12:49:19 -0700

    feat: practice request method delete

--- Course2/first_project/first_app/views.py
+++ Course2/first_project/first_app/views.py
@@ -23,3 +23,13 @@ def list_create_tasks(request) -> HttpResponse :
     task = request.POST.get("task")
     Task.objects.create(name=task)
     return HttpResponse(f"Task Created: '{task}'")
+
+def delete_Task(request , task_id) -> HttpResponse :
+    if request.method == "DELETE" :
+        tasks = Task.objects.filter(id=task_id)
+        if len(tasks) != 0 :
+            task = tasks[0]
+            task.delete()
+            return HttpResponse(f"Task Done: '{task.name}'")
+        else:
+            return HttpResponse(f"There isn't any task with id '{task_id}'")            
